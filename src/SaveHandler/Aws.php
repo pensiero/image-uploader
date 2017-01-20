8676c4c01466a254ac969a59d054ec005e7b3ce3
Luca Mattivi <luca@smartdomotik.com>
2017-01-20 10:54:14 +0100

    Add Docker compatibility (Ubuntu 16.04, PHP7), flysystem and aws

--- src/SaveHandler/Aws.php
+++ src/SaveHandler/Aws.php
@@ -1,8 +1,17 @@
 <?php
 namespace ImageUploader\SaveHandler;
+use Aws\S3\S3Client;
+use ImageUploader\Exception\NotFoundException;
+use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Flysystem\Filesystem;
 
 class Aws implements SaveHandlerInterface
 {
+    public function info($id, $width = null, $height = null)
+    {
+        // TODO: Implement info() method.
+    }
+
     public function save(\Imagick $image, $width = null, $height = null)
     {
         // TODO: Implement save() method.
@@ -10,6 +19,54 @@ class Aws implements SaveHandlerInterface
 
     public function read($id, $width = null, $height = null)
     {
-        // TODO: Implement read() method.
+        // TODO: This should stay in a costruct ?
+        $client = S3Client::factory([
+            'credentials' => [
+                'key'    => getenv('AWS_ACCESS_KEY_ID'),
+                'secret' => getenv('AWS_SECRET_ACCESS_KEY'),
+            ],
+            'region' => getenv('AWS_REGION'),
+            'version' => 'latest|version',
+        ]);
+
+        $adapter = new AwsS3Adapter($client, getenv('AWS_BUCKET'));
+
+        $originalPath = '/original/' . $id;
+        $remotePath = $originalPath;
+        if ($width != null)
+        {
+            $remotePath = '/' . $width . 'x' . $height . '/' . $id;
+        }
+
+
+        // TODO: This should stay in a GenericAdapter (only for images?)
+        if ($adapter->has($remotePath))
+        {
+            $image = $adapter->get($remotePath);
+            return $image;
+        }
+        else if ($adapter->has($originalPath))
+        {
+            $originalFile = $adapter->get($originalPath);
+
+            Image::configure(array('driver' => 'imagick'));
+
+            $editedImage = Image::make($originalFile);
+            $editedImage->fit($width, $height, function ($constraint) {
+                $constraint->upsize();
+            },'center');
+            $editedImage->encode('jpg', 85);
+
+            $resizedRemotePath = '/' . $width . 'x' . $height . '/' . $id;
+            $adapter->put($resizedRemotePath, $editedImage->__toString(), 'public');
+
+            return $editedImage;
+        }
+        else
+        {
+            throw new NotFoundException('This Image cannot be generated because original image doesn\'t exist');
+        }
+
+
     }
 }
