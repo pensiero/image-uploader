5f3310dcd9f9d1f0a421d4d24399d843d2269740
pensiero <oscar.nesis@gmail.com>
2017-02-04 22:23:44 +0100

    Upload form

--- public/upload.php
+++ public/upload.php
@@ -1,4 +1,14 @@
+<h3>Upload your image</h3>
+
 <form method="post" action="index.php" enctype="multipart/form-data">
     <input type="file" name="source" />
+    <br />
+    <br />
+    Width: <input type="number" name="width" />
+    <br />
+    <br />
+    Height: <input type="number" name="height" />
+    <br />
+    <br />
     <button type="submit">Upload</button>
 </form>
