e49a74d089c492e067bf44d16ef3f098b6cd4c9e
pensiero <oscar.nesis@gmail.com>
2017-02-06 20:32:59 +0100

    Cleanup of AWS save handler

--- src/ImageUploader/SaveHandler/Aws.php
+++ src/ImageUploader/SaveHandler/Aws.php
@@ -1,15 +1,26 @@
 <?php
 namespace ImageUploader\SaveHandler;
 
-use Aws\S3\S3Client;
-use ImageUploader\Exception\NotFoundException;
-use League\Flysystem\AwsS3v3\AwsS3Adapter;
-
 class Aws implements SaveHandlerInterface
 {
-    public function info($id, $width = null, $height = null)
+    public function getId()
     {
-        // TODO: Implement info() method.
+        // TODO: Implement getId() method.
+    }
+
+    public function setId($id)
+    {
+        // TODO: Implement setId() method.
+    }
+
+    public function getPath($width = null, $height = null)
+    {
+        // TODO: Implement getPath() method.
+    }
+
+    public function getLocalPath($width = null, $height = null)
+    {
+        // TODO: Implement getLocalPath() method.
     }
 
     public function save(\Imagick $image, $width = null, $height = null)
@@ -17,49 +28,8 @@ class Aws implements SaveHandlerInterface
         // TODO: Implement save() method.
     }
 
-    public function read($id, $width = null, $height = null)
+    public function read($width = null, $height = null)
     {
-        // TODO: This should stay in a costruct ?
-        $client = S3Client::factory([
-            'credentials' => [
-                'key'    => getenv('AWS_ACCESS_KEY_ID'),
-                'secret' => getenv('AWS_SECRET_ACCESS_KEY'),
-            ],
-            'regionzc' => getenv('AWS_REGION'),
-            'version' => 'latest|version',
-        ]);
-
-        $adapter = new AwsS3Adapter($client, getenv('AWS_BUCKET'));
-
-        $originalPath = '/original/' . $id;
-        $remotePath = $originalPath;
-        if ($width != null) {
-            $remotePath = '/' . $width . 'x' . $height . '/' . $id;
-        }
-
-
-        // TODO: This should stay in a GenericAdapter (only for images?)
-        if ($adapter->has($remotePath)) {
-            return $adapter->get($remotePath);
-        }
-
-        if (!$adapter->has($originalPath)) {
-            throw new NotFoundException('This Image cannot be generated because original image doesn\'t exist');
-        }
-
-        $originalFile = $adapter->get($originalPath);
-
-        Image::configure(array('driver' => 'imagick'));
-
-        $editedImage = Image::make($originalFile);
-        $editedImage->fit($width, $height, function ($constraint) {
-            $constraint->upsize();
-        },'center');
-        $editedImage->encode('jpg', 85);
-
-        $resizedRemotePath = '/' . $width . 'x' . $height . '/' . $id;
-        $adapter->put($resizedRemotePath, $editedImage->__toString(), 'public');
-
-        return $editedImage;
+        // TODO: Implement read() method.
     }
 }
